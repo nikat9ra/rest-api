34abeaf91fbe0f4fb382fb923a08b97308d9c7dc
Roman Kanaykin <r.kanaykin@gmail.com>
2022-08-12 13:06:06 +0300

    second lesson

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/nikat9ra/rest-api/internal/app/store"
 	"github.com/sirupsen/logrus"
 )
 
@@ -12,6 +13,7 @@ type APIserver struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+	store  *store.Store
 }
 
 func New(config *Config) *APIserver {
@@ -28,6 +30,10 @@ func (s *APIserver) Start() error {
 	}
 
 	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
 	s.logger.Info("starting api server")
 
 	return http.ListenAndServe(s.config.BindAddr, s.router)
@@ -46,6 +52,17 @@ func (s *APIserver) configureLogger() error {
 
 func (s *APIserver) configureRouter() {
 	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIserver) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+
+	s.store = st
+
+	return nil
 }
 
 func (s *APIserver) handleHello() http.HandlerFunc {
